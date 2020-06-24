457d0cca6c2f05e56731bdf0d98a259e6eff163c
chohaesong <60823814+songrammer@users.noreply.github.com>
2020-06-24 22:13:43 +0900

    update conert_csv_to_JSON

--- convert_csv_to_JSON/convert.js
+++ convert_csv_to_JSON/convert.js
@@ -1,36 +1,19 @@
-const fs= require('fs')
-const path =require('path')
-const csvjson =require('csvjson');
-const { join } = require('path');
-const file='customer-data.csv';
-const new_file='customer-data.json';
-let jsonObj;
-fs.open(file,function(err,data){
-    if(err) console.log("Sorry It's Err")
-    else console.log(` ${file} open Success`)
-    fs.readFile(file,'utf-8',function(error,data){
-        if(error) console.log("Sorry It's read err")
-        //let data_change;
-       
-         jsonObj=csvjson.toObject(data)
-
-        console.log(jsonObj)
-        obj=JSON.stringify(jsonObj)
-        fs.writeFileSync(new_file,obj,'utf-8')
-        
+const csvFilePath = 'customer-data.csv'
+const fs = require('fs')
+const csv = require('csvtojson')
+let arr = []
+csv()
+    .fromFile(csvFilePath)
+    .on('json', (jsonObj)=>{
+        arr.push(jsonObj)
     })
-  
-
-
-    
-
-})
-
-    
-
-
-
-
-
-
-
+    .on('done', (error) =>{
+        if(error) return process.exit(1)
+        console.log(arr)
+        fs.writeFile('customer-data.json', JSON.stringify(arr,null,2), (error) =>{
+            if(error) return process.exit(1)
+            console.log('done')
+            process.exit(0)
+        })
+    }
+)
